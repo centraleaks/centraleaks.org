633c7f86faca522b2b73546dcf13c1ec90a79ed8
E2RD0 <eduardodavidestrada1@gmail.com>
2020-11-24 09:42:31 -0600

    istor function and exit nodes list

--- functions/istor.js
+++ functions/istor.js
@@ -1,13 +1,18 @@
 const torDetect = require('tor-detect');
 
+function isTor(ip) { 
+    return new Promise(function (resolve, reject) { 
+    	torDetect(ip).then(tor => {
+    		resolve(tor); 
+		});
+    }); 
+} 
 exports.handler = async ({ headers }) => {
 	const ip = headers["client-ip"];
- 	torDetect(ip).then(tor => {
-		console.log(tor);
-		return {
-			statusCode: 200,
-			body: tor,
-			headers: { "content-type": "text/html" }
-		};
-	});
+	var tor = await isTor(ip);
+	return {
+	statusCode: 200,
+	body: `[{"ip": "${ip}","istor": ${tor.toString()}}]`,
+	headers: { "content-type": "text/html; charset=UTF-8" }
+	};
 };
